c9bf3ca81e0f891fba746e4db3615a962d71953f
jessrojasal <jessra.93@gmail.com>
2024-11-13 13:20:46 +0100

    getting data

--- 2_getting_data.py
+++ 2_getting_data.py
@@ -1,6 +1,6 @@
 import requests 
 from bs4 import BeautifulSoup 
-#import re 
+from requests import Session
 import json 
 
 
@@ -9,29 +9,69 @@ headers = {
 }
 
 cookie_url = "https://www.immoweb.be"
-def get_cookies(cookie_url):
+def get_cookies():
     req_cookies = requests.get(cookie_url, headers=headers)
     cookies = req_cookies.cookies
     return cookies
 
+def get_script(url):
+     with requests.Session() as session:
+        response = session.get(url, cookies=get_cookies(), headers=headers)
+        soup = BeautifulSoup(response.content, 'html.parser')
+
+        try:
+            scripts = soup.find_all('script', attrs={"type" :"text/javascript"})
+            
+            for script in scripts:
+                script_text = script.string
+                if script_text and 'window.classified' in script_text:
+                    json_str = script_text.strip().replace('window.classified = ', '').rstrip(';')
+                    json_data = json.loads(json_str)
+                    break
+
+            #with open("classified_data.json", "w") as file: 
+                #json.dump(json_data, file, ensure_ascii=False, indent=4)
+                #  
+            return json_data
+        except:
+            pass
+            
+def extracted_dat(url, json_data):
+    property = {   } #initial dictionary
+
+
+
+
+    property["URL"] = url
+    property["ID"] = json_data["id"]
+    property["Locality"] = json_data["property"]["location"]["province"]
+    property["Type of property"] = json_data 
+    property["Subtype of property"] = json_data 
+    property["Type of sale"] = json_data 
+    property["Number of rooms"] = json_data 
+    property["Living Area"] = json_data 
+    property["Fully equipped kitchen"] = json_data 
+    property["Furnished"] = json_data 
+    property["Open fired"] = json_data 
+    property["Terrace"] = json_data 
+    property["Land surface"] = json_data 
+    property["Plot of land surface"] = json_data  
+    property["Number of facades"] = json_data  
+    property["Swimming pool"] = json_data  
+    property["State of the building"] = json_data  
+
+
+
+
+
+
+
+
+
+
+
+
+# Call the function to test
 url = "https://www.immoweb.be/en/classified/apartment/for-sale/wommelgem/2160/20313380"
-response = requests.get(url, cookies=get_cookies(cookie_url), headers=headers)
-soup = BeautifulSoup(response.content, 'html.parser')
+get_script(url)
 
-content = soup.find_all('script', attrs={"type" :"text/javascript"})
-
-for script in content:
-    script_text = script.string
-    #if 'window.classified' in script_text:
-      #  return script_text
-       # json_dic = json.loads(script_text)
-        #return script_text
-
-    print(script_text)
-#with open("javascript", "w") as file: 
-           # json.dump(script_text.string, file, ensure_ascii = False, indent = 4)
-
-
-
-json_str = script_text.strip()
-json_str = json_str.replace('window.classified = ', '')
